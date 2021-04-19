5134f1cb19c5b8379e2a2203a8527824445e5437
Сергей Серебряков <Mir1k1@outlook.com>
2021-04-19 15:36:06 +0300

    1

--- it11/Form1.Designer.cs
+++ it11/Form1.Designer.cs
@@ -31,16 +31,20 @@
       this.pictureBox1 = new System.Windows.Forms.PictureBox();
       this.button1 = new System.Windows.Forms.Button();
       this.button2 = new System.Windows.Forms.Button();
+      this.button3 = new System.Windows.Forms.Button();
+      this.button4 = new System.Windows.Forms.Button();
+      this.button5 = new System.Windows.Forms.Button();
       ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
       this.SuspendLayout();
       // 
       // pictureBox1
       // 
-      this.pictureBox1.Location = new System.Drawing.Point(81, 32);
+      this.pictureBox1.Location = new System.Drawing.Point(97, 54);
       this.pictureBox1.Name = "pictureBox1";
       this.pictureBox1.Size = new System.Drawing.Size(607, 369);
       this.pictureBox1.TabIndex = 0;
       this.pictureBox1.TabStop = false;
+      this.pictureBox1.Click += new System.EventHandler(this.Form1_Load);
       // 
       // button1
       // 
@@ -50,6 +54,7 @@
       this.button1.TabIndex = 1;
       this.button1.Text = "button1";
       this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
       // 
       // button2
       // 
@@ -59,18 +64,54 @@
       this.button2.TabIndex = 2;
       this.button2.Text = "button2";
       this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // button3
+      // 
+      this.button3.Location = new System.Drawing.Point(205, 0);
+      this.button3.Name = "button3";
+      this.button3.Size = new System.Drawing.Size(75, 23);
+      this.button3.TabIndex = 3;
+      this.button3.Text = "button3";
+      this.button3.UseVisualStyleBackColor = true;
+      this.button3.Click += new System.EventHandler(this.button3_Click);
+      // 
+      // button4
+      // 
+      this.button4.Location = new System.Drawing.Point(306, 0);
+      this.button4.Name = "button4";
+      this.button4.Size = new System.Drawing.Size(75, 23);
+      this.button4.TabIndex = 4;
+      this.button4.Text = "button4";
+      this.button4.UseVisualStyleBackColor = true;
+      this.button4.Click += new System.EventHandler(this.button4_Click);
+      // 
+      // button5
+      // 
+      this.button5.Location = new System.Drawing.Point(414, 0);
+      this.button5.Name = "button5";
+      this.button5.Size = new System.Drawing.Size(75, 23);
+      this.button5.TabIndex = 5;
+      this.button5.Text = "button5";
+      this.button5.UseVisualStyleBackColor = true;
+      this.button5.Click += new System.EventHandler(this.button5_Click);
       // 
       // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.button5);
+      this.Controls.Add(this.button4);
+      this.Controls.Add(this.button3);
       this.Controls.Add(this.button2);
       this.Controls.Add(this.button1);
       this.Controls.Add(this.pictureBox1);
       this.Name = "Form1";
       this.Text = "Form1";
       this.Load += new System.EventHandler(this.Form1_Load);
+      this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+      this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
       ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
       this.ResumeLayout(false);
 
@@ -81,6 +122,9 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- it11/Form1.cs
+++ it11/Form1.cs
@@ -9,115 +9,170 @@ using System.Windows.Forms;
 
 namespace it11
 {
-public partial class Form1 :Form
-{  //Объявляем переменные доступные в каждом обработчике события
-private Point PreviousPoint, point; //Точка до перемещения курсора мыши и текущая точка
-private Bitmap bmp;
-  private Pen blackPen;
-private Graphics g;
+  public partial class Form1 : Form
+  {  //Объявляем переменные доступные в каждом обработчике события
+    private Point PreviousPoint, point; //Точка до перемещения курсора мыши и текущая точка
+    private Bitmap bmp;
+    private Pen blackPen;
+    private Graphics g;
 
-  public Form1()
-  {
-    InitializeComponent();
-  }
-
-  private void Form1_Load(object sender, EventArgs e)
-  {
-    blackPen = new Pen(Color.Black, 4); //подготавливаем перо для рисования
-  }
-
-  private void button1_Click(object sender, EventArgs e)
-  {  //открытиефайла
-    OpenFileDialog dialog = new OpenFileDialog(); //описываемипорождаемобъект dialog классаOpenFileDialog
-                                                 //задаемрасширенияфайлов
-    dialog.Filter = "Image files (*.BMP, *.JPG, *.GIF, *.TIF, *.PNG, *.ICO, *.EMF, *.WMF)|*.bmp;*.jpg;*.gif; *.tif; *.png; *.ico; *.emf; *.wmf";
-    if (dialog.ShowDialog() == DialogResult.OK)//вызываем диалоговое окно и проверяем выбран ли файл
+    public Form1()
     {
-      Image image = Image.FromFile(dialog.FileName); //Загружаем в image изображение из выбранного файла
-      int width = image.Width;
-      int height = image.Height;
-      pictureBox1.Width = width;
-      pictureBox1.Height = height;
-
-      bmp = new Bitmap(image, width, height); //создаемизагружаемиз image изображениевформате bmp
-
-      pictureBox1.Image = bmp; //записываем изображение в формате bmp в pictureBox1
-      g = Graphics.FromImage(pictureBox1.Image); //подготавливаем объект Graphics для рисования в pictureBox1
-
+      InitializeComponent();
     }
-  }
 
-  private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
-  { // обработчик события нажатия кнопки на мыши
-    // записываем в предыдущую точку (PreviousPoint) текущие координаты
-    PreviousPoint.X = e.X;
-    PreviousPoint.Y = e.Y;
-  }
-
-
-  private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
-  {//Обработчик события перемещения мыши по pictuteBox1
-    if (e.Button == MouseButtons.Left) //Проверяем нажата ли левая кнопка мыши
-    {  //запоминаем в point текущее положение курсора мыши
-      point.X = e.X;
-      point.Y = e.Y;
-
-      //соеденяем линией предыдущую точку с текущей
-      g.DrawLine(blackPen, PreviousPoint, point);
-
-      //текущее положение курсора мыши сохраняем в PreviousPoint
-      PreviousPoint.X = point.X;
-      PreviousPoint.Y = point.Y;
-      pictureBox1.Invalidate();//Принудительновызываемпереррисовку pictureBox1
-    }
-  }
-
-  private void button2_Click(object sender, EventArgs e)
-  { //сохранениефайла
-    SaveFileDialog savedialog = new SaveFileDialog();//описываемипорождаемобъектsavedialog
-                                                   //задаем свойства для savedialog
-    savedialog.Title = "Сохранить картинку как ...";
-    savedialog.OverwritePrompt = true;
-    savedialog.CheckPathExists = true;
-    savedialog.Filter =
-    "Bitmap File(*.bmp)|*.bmp|" +
-    "GIF File(*.gif)|*.gif|" +
-    "JPEG File(*.jpg)|*.jpg|" +
-    "TIF File(*.tif)|*.tif|" +
-    "PNG File(*.png)|*.png";
-    savedialog.ShowHelp = true;
-    // If selected, save
-    if (savedialog.ShowDialog() == DialogResult.OK)//вызываемдиалоговоеокноипроверяемзаданолиимяфайла
+    private void Form1_Load(object sender, EventArgs e)
     {
-      // в строку fileName записываем указанный в savedialog полный путь к файлу
-      string fileName = savedialog.FileName;
-      // Убираем из имени три последних символа (расширение файла)
-      string strFilExtn =
-      fileName.Remove(0, fileName.Length - 3);
-      // Сохраняем файл в нужном формате и с нужным расширением
-      switch (strFilExtn)
+      blackPen = new Pen(Color.Black, 4); //подготавливаем перо для рисования
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {  //открытиефайла
+      OpenFileDialog dialog = new OpenFileDialog(); //описываемипорождаемобъект dialog классаOpenFileDialog
+                                                    //задаемрасширенияфайлов
+      dialog.Filter = "Image files (*.BMP, *.JPG, *.GIF, *.TIF, *.PNG, *.ICO, *.EMF, *.WMF)|*.bmp;*.jpg;*.gif; *.tif; *.png; *.ico; *.emf; *.wmf";
+      if (dialog.ShowDialog() == DialogResult.OK)//вызываем диалоговое окно и проверяем выбран ли файл
       {
-        case "bmp":
-          bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Bmp);
-          break;
-        case "jpg":
-          bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Jpeg);
-          break;
-        case "gif":
-          bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Gif);
-          break;
-        case "tif":
-          bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Tiff);
-          break;
-        case "png":
-          bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Png);
-          break;
-        default:
-          break;
+        Image image = Image.FromFile(dialog.FileName); //Загружаем в image изображение из выбранного файла
+        int width = image.Width;
+        int height = image.Height;
+        pictureBox1.Width = width;
+        pictureBox1.Height = height;
+
+        bmp = new Bitmap(image, width, height); //создаемизагружаемиз image изображениевформате bmp
+
+        pictureBox1.Image = bmp; //записываем изображение в формате bmp в pictureBox1
+        g = Graphics.FromImage(pictureBox1.Image); //подготавливаем объект Graphics для рисования в pictureBox1
+
+      }
+      button5.Enabled = true;
+      button4.Enabled = true;
+      button3.Enabled = true;
+    }
+
+    private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+    { // обработчик события нажатия кнопки на мыши
+      // записываем в предыдущую точку (PreviousPoint) текущие координаты
+      PreviousPoint.X = e.X;
+      PreviousPoint.Y = e.Y;
+    }
+
+
+    private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+    {//Обработчик события перемещения мыши по pictuteBox1
+      if (e.Button == MouseButtons.Left) //Проверяем нажата ли левая кнопка мыши
+      {  //запоминаем в point текущее положение курсора мыши
+        point.X = e.X;
+        point.Y = e.Y;
+
+        //соеденяем линией предыдущую точку с текущей
+        g.DrawLine(blackPen, PreviousPoint, point);
+
+        //текущее положение курсора мыши сохраняем в PreviousPoint
+        PreviousPoint.X = point.X;
+        PreviousPoint.Y = point.Y;
+        pictureBox1.Invalidate();//Принудительновызываемпереррисовку pictureBox1
       }
     }
+    private void button3_Click(object sender, EventArgs e)
+    {
+       //циклы для перебора всех пикселей на изображении
+        for (int i = 0; i < bmp.Width; i++)
+          for (int j = 0; j < bmp.Height; j++)
+          {
+            int R = bmp.GetPixel(i, j).R; //извлекаем в R значение красного цвета в текущей точке
+           
+           
+            Color p = Color.FromArgb(255,R,0,0); //переводим int в значение цвета. 255 - показывает степень прозрачности. остальные значения одинаковы для трех каналов R,G,B
+            bmp.SetPixel(i, j, p); //записываме полученный цвет в текущую точку
+          }
+        Refresh(); //вызываем функцию перерисовки окна
+      button5.Enabled = false;
+      button4.Enabled = false;
+      button3.Enabled = false;
+
+    }
+
+    private void button4_Click(object sender, EventArgs e)
+    {
+      for (int i = 0; i < bmp.Width; i++)
+        for (int j = 0; j < bmp.Height; j++)
+        {
+          int G = bmp.GetPixel(i, j).G; //извлекаем в R значение красного цвета в текущей точке
+
+
+          Color p = Color.FromArgb(255, 0, G, 0); //переводим int в значение цвета. 255 - показывает степень прозрачности. остальные значения одинаковы для трех каналов R,G,B
+          bmp.SetPixel(i, j, p); //записываме полученный цвет в текущую точку
+        }
+      Refresh(); //вызываем функцию перерисовки окна
+      button5.Enabled = false;
+      button4.Enabled = false;
+      button3.Enabled = false;
+    }
+
+    private void button5_Click(object sender, EventArgs e)
+    {
+      for (int i = 0; i < bmp.Width; i++)
+        for (int j = 0; j < bmp.Height; j++)
+        {
+          int B = bmp.GetPixel(i, j).B; //извлекаем в R значение красного цвета в текущей точке
+
+
+          Color p = Color.FromArgb(255, 0, 0, B); //переводим int в значение цвета. 255 - показывает степень прозрачности. остальные значения одинаковы для трех каналов R,G,B
+          bmp.SetPixel(i, j, p); //записываме полученный цвет в текущую точку
+        }
+      Refresh(); //вызываем функцию перерисовки окна
+      button5.Enabled = false;
+      button4.Enabled = false;
+      button3.Enabled = false;
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    { //сохранениефайла
+      SaveFileDialog savedialog = new SaveFileDialog();//описываемипорождаемобъектsavedialog
+                                                       //задаем свойства для savedialog
+      savedialog.Title = "Сохранить картинку как ...";
+      savedialog.OverwritePrompt = true;
+      savedialog.CheckPathExists = true;
+      savedialog.Filter =
+      "Bitmap File(*.bmp)|*.bmp|" +
+      "GIF File(*.gif)|*.gif|" +
+      "JPEG File(*.jpg)|*.jpg|" +
+      "TIF File(*.tif)|*.tif|" +
+      "PNG File(*.png)|*.png";
+      savedialog.ShowHelp = true;
+      // If selected, save
+      if (savedialog.ShowDialog() == DialogResult.OK)//вызываемдиалоговоеокноипроверяемзаданолиимяфайла
+      {
+        // в строку fileName записываем указанный в savedialog полный путь к файлу
+        string fileName = savedialog.FileName;
+        // Убираем из имени три последних символа (расширение файла)
+        string strFilExtn =
+        fileName.Remove(0, fileName.Length - 3);
+        // Сохраняем файл в нужном формате и с нужным расширением
+        switch (strFilExtn)
+        {
+          case "bmp":
+            bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Bmp);
+            break;
+          case "jpg":
+            bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+            break;
+          case "gif":
+            bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Gif);
+            break;
+          case "tif":
+            bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Tiff);
+            break;
+          case "png":
+            bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Png);
+            break;
+          default:
+            break;
+        }
+      }
+    }
+
+
   }
-
-
-}
 }
